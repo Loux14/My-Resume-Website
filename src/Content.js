25372063d1fbee01535805e68d64572e0422b70e
Loux14 <122696881+Loux14@users.noreply.github.com>
2024-06-25 23:08:48 -0400

    photo prizes column

--- src/Content.js
+++ src/Content.js
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
-import Introduction from './Introduction';
-import Resume from './Resume';
-import Projects from "./Projects"
-import References from './References'
-import Contacts from './Contacts'
-import Skills from './Skills'
-import Notable from './Notable'
-import Teaching from './Teaching'
-import './App.css'
+import React, { useState } from "react";
+import Introduction from "./Introduction";
+import Resume from "./Resume";
+import Projects from "./Projects";
+import References from "./References";
+import Contacts from "./Contacts";
+import Skills from "./Skills";
+import Notable from "./Notable";
+import Teaching from "./Teaching";
+import "./App.css";
 
 function Content() {
-  const [selectedItem, setSelectedItem] = useState('Introduction');
+  const [selectedItem, setSelectedItem] = useState("Introduction");
   const [menuOpen, setMenuOpen] = useState(false);
 
   const handleItemClick = (item) => {
     setSelectedItem(item);
-    setMenuOpen(false); 
+    setMenuOpen(false);
   };
 
   const toggleMenu = () => {
@@ -25,30 +25,74 @@ function Content() {
   return (
     <div className="Content">
       <div className="TopPanel">
-      <div className="NameMenu"><a href="http://www.lucas-g.ca" rel="noopener noreferrer">Lucas G</a></div>
+        <div className="NameMenu">
+          <a href="http://www.lucas-g.ca" rel="noopener noreferrer">
+            Lucas G
+          </a>
+        </div>
         <div className="menu-icon" onClick={toggleMenu}>
           ☰
         </div>
-        <ul className={menuOpen ? 'show' : ''}>
-          <li className={selectedItem === 'Introduction' ? 'active' : ''} onClick={() => handleItemClick('Introduction')}>Introduction</li>
-          <li className={selectedItem === 'Skills' ? 'active' : ''} onClick={() => handleItemClick('Skills')}>Skills</li>
-          <li className={selectedItem === 'Projects' ? 'active' : ''} onClick={() => handleItemClick('Projects')}>Projects</li>
-          <li className={selectedItem === 'Resume' ? 'active' : ''} onClick={() => handleItemClick('Resume')}>Documents</li>
-          <li className={selectedItem === 'Notable' ? 'active' : ''} onClick={() => handleItemClick('Notable')}>Prizes</li>
-          <li className={selectedItem === 'Teaching' ? 'active' : ''} onClick={() => handleItemClick('Teaching')}>Teaching</li>
-          <li className={selectedItem === 'References' ? 'active' : ''} onClick={() => handleItemClick('References')}>References</li>
-          <li className={selectedItem === 'Contact' ? 'active' : ''} onClick={() => handleItemClick('Contact')}>Contact</li>
+        <ul className={menuOpen ? "show" : ""}>
+          <li
+            className={selectedItem === "Introduction" ? "active" : ""}
+            onClick={() => handleItemClick("Introduction")}
+          >
+            Introduction
+          </li>
+          <li
+            className={selectedItem === "Skills" ? "active" : ""}
+            onClick={() => handleItemClick("Skills")}
+          >
+            Skills
+          </li>
+          <li
+            className={selectedItem === "Projects" ? "active" : ""}
+            onClick={() => handleItemClick("Projects")}
+          >
+            Projects
+          </li>
+          <li
+            className={selectedItem === "Resume" ? "active" : ""}
+            onClick={() => handleItemClick("Resume")}
+          >
+            Documents
+          </li>
+          <li
+            className={selectedItem === "Notable" ? "active" : ""}
+            onClick={() => handleItemClick("Notable")}
+          >
+            Prizes
+          </li>
+          <li
+            className={selectedItem === "Teaching" ? "active" : ""}
+            onClick={() => handleItemClick("Teaching")}
+          >
+            Teaching
+          </li>
+          <li
+            className={selectedItem === "References" ? "active" : ""}
+            onClick={() => handleItemClick("References")}
+          >
+            References
+          </li>
+          <li
+            className={selectedItem === "Contact" ? "active" : ""}
+            onClick={() => handleItemClick("Contact")}
+          >
+            Contact
+          </li>
         </ul>
       </div>
       <div className="MainContent">
-        {selectedItem === 'Introduction' && <Introduction />}
-        {selectedItem === 'Resume' && <Resume />}
-        {selectedItem === 'Projects' && <Projects />}
-        {selectedItem === 'Skills' && <Skills />}
-        {selectedItem === 'Notable' && <Notable />}
-        {selectedItem === 'Teaching' && <Teaching />}
-        {selectedItem === 'References' && <References />}
-        {selectedItem === 'Contact' && <Contacts />}
+        {selectedItem === "Introduction" && <Introduction />}
+        {selectedItem === "Resume" && <Resume />}
+        {selectedItem === "Projects" && <Projects />}
+        {selectedItem === "Skills" && <Skills />}
+        {selectedItem === "Notable" && <Notable />}
+        {selectedItem === "Teaching" && <Teaching />}
+        {selectedItem === "References" && <References />}
+        {selectedItem === "Contact" && <Contacts />}
       </div>
     </div>
   );
--- src/Notable.js
+++ src/Notable.js
@@ -1,30 +1,46 @@
-import React from 'react';
-import './Notable.css';
+import React from "react";
+import "./Notable.css";
 
 function Notable() {
-    return (
-        <div className="Notable">
-
-            <div className="TextZoneNotable">
-                <h1>First Prize - uHack 2023 - Hackathon</h1>
-                <h2>On the weekend of March 19th 2023, I was the leader of the winning team in UQO’s Hackathon “UHack”.</h2>
-                <div className="PhotoNotable">
-                    <img src="victory.jpg" alt="uHack23" />
-                    <img src="creddowebsite.jpg" alt="creddo" />
-                </div>
-             
-                <p>We had 2 days, in a team of 5, to think and make (as much as we could) a technologic solution to help one of three problems given. I naturally took the lead of my team and proposed a website.
-                    The problem to solve, was to help the CREDDO communicate with local cities about wind and solar energies possibilities on their land.
-                    The ressources given were geomatic datas about wind and sun, maps with road and lands, but we had to find the other information necessary to install panels or turbines.
-                    I was the one in charge of writing the website’s code.</p>
-                <p>Then we created an interactive website to show each citizen the possibilities of alternatives energies on their lands.
-
-                    That day, we each won a 2 nights stay at Dômes Outaouais.</p>
-                <a href="https://uqo.ca/nouvelles/50536" target="_blank" rel="noreferrer" >https://uqo.ca/nouvelles/50536</a>
-            </div>
-
+  return (
+    <div className="Notable">
+      <div className="TextZoneNotable">
+        <h1>First Prize - uHack 2023 - Hackathon</h1>
+        <h2>
+          On the weekend of March 19th 2023, I was the leader of the winning
+          team in UQO’s Hackathon “UHack”.
+        </h2>
+        <div className="PhotoNotable">
+          <img src="victory.jpg" alt="uHack23" />
+          <img src="creddowebsite.jpg" alt="creddo" />
         </div>
-    );
+
+        <p>
+          We had 2 days, in a team of 5, to think and make (as much as we could)
+          a technologic solution to help one of three problems given. I
+          naturally took the lead of my team and proposed a website. The problem
+          to solve, was to help the CREDDO communicate with local cities about
+          wind and solar energies possibilities on their land. The ressources
+          given were geomatic datas about wind and sun, maps with road and
+          lands, but we had to find the other information necessary to install
+          panels or turbines. I was the one in charge of writing the website’s
+          code.
+        </p>
+        <p>
+          Then we created an interactive website to show each citizen the
+          possibilities of alternatives energies on their lands. That day, we
+          each won a 2 nights stay at Dômes Outaouais.
+        </p>
+        <a
+          href="https://uqo.ca/nouvelles/50536"
+          target="_blank"
+          rel="noreferrer"
+        >
+          https://uqo.ca/nouvelles/50536
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Notable;
