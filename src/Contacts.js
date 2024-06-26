926f0f66f3596c191c46309ae83a93eed1ed51f3
Loux14 <122696881+Loux14@users.noreply.github.com>
2024-06-26 11:47:29 -0400

    margin burger, margin documents, phone number and email link

--- src/Contacts.js
+++ src/Contacts.js
@@ -1,17 +1,57 @@
-import React from 'react';
-import './Contacts.css'; 
+import React from "react";
+import "./Contacts.css";
 
 function Contacts() {
   return (
     <div className="contacts-container">
       <h1>Contact</h1>
       <div className="contact-info">
-        <p><strong>Phone</strong> <a href="tel:+1234567890"> 819 351 2327</a></p>
-        <p><strong>Email</strong> <a href="mailto:youremail@example.com" target="_blank" rel="noopener noreferrer">lucas.gourlia@pm.me</a></p><br/>
-        <p><strong>WebSite</strong> <a href="http://www.lucas-g.ca" target="_blank" rel="noopener noreferrer">www.lucas-g.ca</a></p>
-        <p><strong>LinkedIn</strong> <a href="https://www.linkedin.com/in/lucas-g-b28884306/" target="_blank" rel="noopener noreferrer">Lucas G.</a></p><br/>
+        <p>
+          <strong>Phone</strong> <a href="tel:+18193512327"> 819 351 2327</a>
+        </p>
+        <p>
+          <strong>Email</strong>{" "}
+          <a
+            href="mailto:lucas.gourlia@pm.me"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            lucas.gourlia@pm.me
+          </a>
+        </p>
+        <br />
+        <p>
+          <strong>WebSite</strong>{" "}
+          <a
+            href="http://www.lucas-g.ca"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            www.lucas-g.ca
+          </a>
+        </p>
+        <p>
+          <strong>LinkedIn</strong>{" "}
+          <a
+            href="https://www.linkedin.com/in/lucas-g-b28884306/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Lucas G.
+          </a>
+        </p>
+        <br />
 
-        <p><strong>GitHub</strong> <a href="https://github.com/Loux14" target="_blank" rel="noopener noreferrer">Loux14</a></p>
+        <p>
+          <strong>GitHub</strong>{" "}
+          <a
+            href="https://github.com/Loux14"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Loux14
+          </a>
+        </p>
       </div>
     </div>
   );
--- src/Resume.js
+++ src/Resume.js
@@ -1,29 +1,33 @@
-import React from 'react';
-import './Resume.css';
+import React from "react";
+import "./Resume.css";
 
 function Resume() {
   const downloadPdf = (pdfUrl) => {
-    const link = document.createElement('a');
+    const link = document.createElement("a");
     link.href = pdfUrl;
-    link.download = pdfUrl.split('/').pop(); // Télécharge le fichier avec son nom
+    link.download = pdfUrl.split("/").pop(); // Télécharge le fichier avec son nom
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
   };
 
   return (
-    <div className='Resume'>
-      <div className='block-info'>
+    <div className="Resume">
+      <div className="block-info">
         <div>
-          <button onClick={() => downloadPdf('Lucas_G_2024.pdf')}>Download Resume</button>
+          <button onClick={() => downloadPdf("Lucas_G_2024.pdf")}>
+            Download Resume
+          </button>
         </div>
         <div className="PhotoResume">
           <img src="Resume_Pic_LucasG_2024.png" alt="Resume" />
         </div>
       </div>
-      <div className='block-info'>
+      <div className="block-info">
         <div>
-          <button onClick={() => downloadPdf('results.pdf')}>Download University Results</button>
+          <button onClick={() => downloadPdf("results.pdf")}>
+            Download University Results
+          </button>
         </div>
         <div className="PhotoResume">
           <img src="results.png" alt="Results" />
