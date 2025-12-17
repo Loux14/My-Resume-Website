c47cb3cc8c2ccb9e3a283901b963153c16955b19
Lucas <goul20@uqo.ca>
2025-12-17 16:52:52 -0500

    not much

--- src/Skills.js
+++ src/Skills.js
@@ -3,20 +3,20 @@ import './Skills.css';
 function Skills() {
   
   const skills = [
-    { id: 2, title: 'Java', description: 'Description de la compétence 2', xp: '18 months', level: 'Advanced', image: 'java.png' },
-    { id: 1, title: 'JavaScript', description: 'Description de la compétence 1', xp: '2 years', level: 'Intermediate', image: 'js.png' },
+    { id: 2, title: 'Java', description: 'Description de la compétence 2', xp: '3 years', level: 'Advanced', image: 'java.png' },
+    { id: 1, title: 'JavaScript', description: 'Description de la compétence 1', xp: '3 years', level: 'Intermediate', image: 'js.png' },
     { id: 4, title: 'React.js', description: 'Description de la compétence 1', xp: '6 months', level: 'Beginner', image: 'react.png' },
     { id: 6, title: 'C', description: 'Description de la compétence 3', xp: '3 months', level: 'Beginner', image: 'c.png' },
 
-    { id: 12, title: 'HTML', description: 'Description de la compétence 3', xp: '2 years', level: 'Beginner', image: 'html.png' },
+    { id: 12, title: 'HTML', description: 'Description de la compétence 3', xp: '3 years', level: 'Intermediate', image: 'html.png' },
     { id: 9, title: 'PHP', description: 'Description de la compétence 3', xp: '3 months', level: 'Novice', image: 'php.png' },
-    { id: 10, title: 'Reac Native', description: 'Description de la compétence 3', xp: '6 months', level: 'Beginner', image: 'react-native.png' },
-    { id: 11, title: 'Swift', description: 'Description de la compétence 3', xp: '6 months', level: 'Beginner', image: 'swift.png' },
+    { id: 10, title: 'React Native', description: 'Description de la compétence 3', xp: '6 months', level: 'Beginner', image: 'react-native.png' },
+    { id: 11, title: 'Swift', description: 'Description de la compétence 3', xp: '1 year', level: 'Intermediate', image: 'swift.png' },
 
 
     { id: 5, title: 'MySQL', description: 'Description de la compétence 2', xp: '1 year', level: 'Beginner', image: 'mysql.png' },
     { id: 3, title: 'Kali', description: 'Description de la compétence 3', xp: '3 years', level: 'Moderate', image: 'kali.png' },
-    { id: 7, title: 'Git', description: 'Description de la compétence 3', xp: '1 year', level: 'Beginner', image: 'git.png' },
+    { id: 7, title: 'Git', description: 'Description de la compétence 3', xp: '3 year', level: 'Intermediate', image: 'git.png' },
     { id: 8, title: 'Bash', description: 'Description de la compétence 3', xp: '2 years', level: 'Beginner', image: 'linux.png' },
 
   ];
